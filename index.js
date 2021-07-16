ef238d75af662b86f2eec09b61d7abf24289ae1d
trantrung76 <85749782+trantrung76@users.noreply.github.com>
2021-07-16 14:23:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
-    response.end("Hello Friday!");
+    response.end("Hello Jenkins Friday!");
 });
 
 const port = process.env.PORT || 1337;
